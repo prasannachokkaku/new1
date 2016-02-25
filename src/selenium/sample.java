b5ac4576d1525f49e1471b0baf98495bd49c2418
hiiii <rahul.lucky25@gmail.com>
2016-02-25 17:22:13 +0530

    hhhh

---
+++ src/selenium/sample.java
@@ -0,0 +1,13 @@
+package selenium;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class sample {
+
+	public static void main(String[] args) {
+		
+FirefoxDriver driver=new FirefoxDriver();
+driver.get("https://www.google.co.in/");
+	}
+
+}
